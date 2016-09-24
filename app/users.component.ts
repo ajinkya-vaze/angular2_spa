bdcde9883a53f0ffab53f68b50f6d7a2dfffcc44
Ajinkya Vaze <ajinkyav@gmail.com>
2016-09-24 12:11:21 +0530

    Removed fontawesome, isn't going well with zone js

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
-    template: `
-    <h1>Users</h1>
-    `
+    templateUrl: '/app/users.component.html'
 })
-export class UsersComponent { }
+export class UsersComponent implements OnInit {
+    users: any[];
+    isLoading: boolean = true;
+
+    constructor(private _userService: UserService) {
+
+    }
+
+    ngOnInit() {
+        this._userService.getUsers().subscribe(
+            response => {
+                this.users = response.json();
+            },
+            null,
+            () => {
+                this.isLoading = false;
+            });
+    }
+}
